3b98bb164b97c3e2f61bce74583ab5456c97fd0d
rachid elaid <rachidelaid@gmail.com>
2022-04-07 14:22:30 +0000

    add classroom and change student class

---
+++ classroom.rb
@@ -0,0 +1,13 @@
+class Classroom
+  attr_accessor :label, :students
+
+  def initialize(label)
+    @label = label
+    @student = []
+  end
+
+  def add_student(student)
+    @student.push(student)
+    student.classroom = self
+  end
+end
