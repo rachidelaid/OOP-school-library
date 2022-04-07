86467acbf9048020380a4e437313b12496f14af5
rachid elaid <rachidelaid@gmail.com>
2022-04-07 15:57:40 +0000

    added UI

--- teacher.rb
+++ teacher.rb
@@ -3,9 +3,9 @@ require_relative('./person')
 class Teacher < Person
   attr_accessor :specialization
 
-  def initialize(specialization, age, name, parent_permission)
+  def initialize(specialization, age, name = "Unkown", parent_permission:true)
+    super(age, name, parent_permission:parent_permission)
     @specialization = specialization
-    super(age, name, parent_permission)
   end
 
   def can_use_services?
